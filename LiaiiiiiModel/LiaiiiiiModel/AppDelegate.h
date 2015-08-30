fdbe118c5e60a562a218bafa7604bbe6a4a49678
xiaoli <xiaoli@126.com>
2015-08-30 17:05:37 +0800

    初始化一个框架

---
+++ LiaiiiiiModel/LiaiiiiiModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiaiiiiiModel
+//
+//  Created by Apple on 15/8/30.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiaiiiiiModel/LiaiiiiiModel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LiaiiiiiModel
+//
+//  Created by Apple on 15/8/30.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
